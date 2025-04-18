dbb72e30ec0f52baf0d6ee29e59aa20393071be8
Froggo23 <81355190+Froggo23@users.noreply.github.com>
2025-04-18 21:07:46 +0900

    build

--- src/main/java/ian/choe/rankmyart/ArtworkController.java
+++ src/main/java/ian/choe/rankmyart/ArtworkController.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.WebUtils;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
